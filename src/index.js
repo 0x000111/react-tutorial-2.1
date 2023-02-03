e1568a329a21ae1e671dc61663911b5a9a8e8d69
0x000111 <38901889+0x000111@users.noreply.github.com>
2023-02-03 10:49:05 +0800

    add change event to checkbox

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  changeHandler = () => {
+    console.log('checkbox value changed!');
+  };
   render() {
     return (
       <div>
-        <input tabIndex="1" type="checkbox" name="chkbox" id="checkbox" />
+        <input
+          tabIndex="1"
+          type="checkbox"
+          name="chkbox"
+          id="checkbox"
+          onChange={this.changeHandler}
+        />
         <label htmlFor="checkbox" className="checkbox-label">
           check me
         </label>
